7e5950717eb7310b29eb75a644c9874e7e24ab5d
OrMoshe <ormoshe2468@gmail.com>
2024-05-11 14:27:34 +0300

    use the model

---
+++ nlp/README.txt
@@ -0,0 +1,4 @@
+/models: Store your machine learning or NLP models here, either as Python files or saved model files.
+/scripts: Include scripts for training the models, evaluating them, and any other automation you might need.
+/utils: Helper functions and utilities that are used across the project (e.g., text cleaning, data loading functions).
+/data: Optional, for storing sample data or data needed for testing.
---
+++ nlp/scripts/execute.py
@@ -0,0 +1,12 @@
+from transformers import pipeline, BertForSequenceClassification, BertTokenizerFast
+
+
+model_path = "../models/torch-bert-base-uncased-model"
+
+model = BertForSequenceClassification.from_pretrained(model_path)
+tokenizer= BertTokenizerFast.from_pretrained(model_path)
+nlp= pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+text = "Recently, I've been feeling very anxious and overwhelmed at work. It's started to affect my sleep and my relationships. I don't feel like it's an emergency, but I could really use some advice on how to manage this stress."
+
+print(nlp(text))
